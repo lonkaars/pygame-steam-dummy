9bae96bfbc692bb2b452b287dde4232f84a392ad
lonkaars <l.leblansch@gmail.com>
2021-02-11 08:53:22 +0100

    updated readme

--- main.py
+++ main.py
@@ -10,9 +10,6 @@ def load_fonts():
             pygame.font.Font("font/SemiBold.ttf", 12)
             )
 
-def percentage_between(value, smallest, largest):
-    return (value - smallest) / (largest - smallest)
-
 class steam_message:
     def __init__(self, **kwargs):
         self.author = kwargs["author"]
@@ -35,6 +32,9 @@ class steam_message:
         self.window.blit(gradients.vertical((34, 34), (108, 137, 56, 255), (91, 118, 53, 255)), [p+20 for p in self.position])
         self.window.blit(pygame.transform.scale(self.avatar, (32, 32)), (self.position[0] + 21, self.position[1] + 21))
 
+    def linear_interpolate(self, value, smallest, largest):
+        return (value - smallest) / (largest - smallest)
+
     def animation_interpolation(self, frame):
         relative_frame = frame - self.starting_frame
         keyframe_1 = int(self.framerate * 0.2)
@@ -42,11 +42,11 @@ class steam_message:
         keyframe_3 = keyframe_2 + int(self.framerate * 0.2)
 
         if relative_frame in range(0, keyframe_1):
-            return percentage_between(relative_frame, 0, keyframe_1)
+            return self.linear_interpolate(relative_frame, 0, keyframe_1)
         elif relative_frame in range(keyframe_1, keyframe_2):
             return 1
         elif relative_frame in range(keyframe_2, keyframe_3):
-            return percentage_between(relative_frame, keyframe_3, keyframe_2)
+            return self.linear_interpolate(relative_frame, keyframe_3, keyframe_2)
         else:
             return 0
 
--- readme.md
+++ readme.md
@@ -4,5 +4,36 @@ This is a simple proof-of-concept steam message dummy for PyGame. This is also m
 
 ![](screenshot.png)
 
-*Will update later with how to use
+## How to use in your own project
+
+Rename main.py to steam_message.py and copy it and gradients.py into your own project folder.
+
+```py
+from steam_message import steam_message
+
+...
+
+# initialize the message
+msg = steam_message(
+		author = string,
+		message = string,
+		avatar = pygame.Surface,
+		fonts = pygame.Font[3],
+		frame = int,
+		framerate = int,
+		window = Surface
+		)
+
+# show message after 5 seconds
+msg.starting_frame = 5 * framerate
+
+...
+
+while True: # drawloop
+	# pygame boilerplate code left out
+
+	msg.render(int(current_frame)) # call at the end of your drawloop to draw on top of everything else
+```
+
+Example usage is provided in `main.py > main()`
 
